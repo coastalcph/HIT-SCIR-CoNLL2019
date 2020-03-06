bb6c8c2bd713a19b448ae7d8020fd5c7f03229e6
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-03-06 15:29:03 +0100

    updated script to send jobs over the wk

--- bash/train_ud.sh
+++ bash/train_ud.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=eud
+#SBATCH --job-name=eud_$2
 #number of independent tasks we are going to start in this script
 #SBATCH --ntasks=1
 #number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=1
+##SBATCH -p gpu --gres=gpu:titanx:1
 #We expect that our program should not run longer than 30 min
 #Note that a program will be killed once it exceeds this time! 
-#SBATCH --time=0-05:00:00
+#SBATCH --time=2-00:00:00
 #Skipping many options! see man sbatch
 # From here on, we can start our program
 
+#hostname
+#echo $CUDA_VISIBLE_DEVICES
+#conda activate hit_scir
 # examples of training commands
 lang=$1
 iso=$2
-checkpoint_dir=checkpoints/test
+checkpoint_dir=$3
 
 rm -rf $checkpoint_dir
 TRAIN_PATH=/image/nlp-datasets/iwpt20/train-dev/$lang/$iso-ud-train.conllu \
