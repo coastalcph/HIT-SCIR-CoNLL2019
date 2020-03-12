2e584a4fee0299e25b97ee713d01eb900b948368
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-03-12 16:15:58 +0100

    using multilingual bert

--- bash/train_ud.sh
+++ bash/train_ud.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=eud_$2
-#number of independent tasks we are going to start in this script
 #SBATCH --ntasks=1
-#number of cpus we want to allocate for each program
 #SBATCH --cpus-per-task=1
 ##SBATCH -p gpu --gres=gpu:titanx:1
-#We expect that our program should not run longer than 30 min
-#Note that a program will be killed once it exceeds this time! 
 #SBATCH --time=2-00:00:00
-#Skipping many options! see man sbatch
-# From here on, we can start our program
 
-#hostname
 #echo $CUDA_VISIBLE_DEVICES
 #conda activate hit_scir
 # examples of training commands
@@ -22,10 +14,10 @@ checkpoint_dir=$3
 rm -rf $checkpoint_dir
 TRAIN_PATH=/image/nlp-datasets/iwpt20/train-dev/$lang/$iso-ud-train.conllu \
 DEV_PATH=/image/nlp-datasets/iwpt20/train-dev/$lang/$iso-ud-dev.conllu \
-WORD_DIM=1024 \
+WORD_DIM=768 \
 LOWER_CASE=FALSE \
 BATCH_SIZE=4 \
-BERT_PATH=/image/nlp-letre/bert/wwm_cased_L-24_H-1024_A-16/ \
+BERT_PATH=/image/nlp-letre/bert/multi_cased_L-12_H-768_A-12/ \
 allennlp train \
 -s $checkpoint_dir \
 --include-package utils \
