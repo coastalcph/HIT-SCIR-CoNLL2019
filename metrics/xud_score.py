00a3e5cbe2717c72a969f4deecfa42cba5e2eb08
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-03-13 15:11:39 +0100

    allow NODE action from root

--- metrics/xud_score.py
+++ metrics/xud_score.py
@@ -28,6 +28,8 @@ class XUDScore(Metric):
         golds : the gold graph
         """
         #hack to avoid double new lines when joining
+        #TODO: this code is ugly and possibly suboptimal
+        #predictions
         predictions = [pred if pred != '\n' else '' for pred in predictions]
         string_pred = '\n'.join(predictions)+'\n'
         string_pred = string_pred.encode("utf-8")
@@ -38,15 +40,9 @@ class XUDScore(Metric):
         pred_collapsed = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True,
                 check=True)
         preds = [pred for pred in pred_collapsed.stdout.partition("\n") ]
-        try:
-            pred_graphs = load_conllu_default(string_to_file(''.join(preds)))
-        except UDError:
-            print('string pred')
-            print(string_pred)
-            print('preds')
-            print(preds)
-            print(pred_tmp_file.name)
-            return
+        pred_graphs = load_conllu_default(string_to_file(''.join(preds)))
+
+        #gold
         golds = [gold if gold != '\n' else '' for gold in golds]
         string_gold = '\n'.join(golds)+'\n'
         string_gold = string_gold.encode("utf-8")
@@ -57,16 +53,7 @@ class XUDScore(Metric):
         gold_collapsed = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True,
                 check=True)
         golds_c = [gold for gold in gold_collapsed.stdout.partition("\n")]
-        try:
-            gold_graphs = load_conllu_default(string_to_file(''.join(golds_c)))
-        except UDError:
-            print('sting gold')
-            print(string_gold)
-            print('golds_c')
-            print(golds_c)
-            print(gold_tmp_file.name)
-            return
-
+        gold_graphs = load_conllu_default(string_to_file(''.join(golds_c)))
         self.results.append(evaluate(gold_graphs, pred_graphs))
 
     def get_metric(self, reset: bool = False) -> Dict[str,float]:
