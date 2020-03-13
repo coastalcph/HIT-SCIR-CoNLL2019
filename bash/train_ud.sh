8420cf27541141c4d2193e4421595851354d4d23
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-03-13 15:15:16 +0100

    using distilled bert and gpu

--- bash/train_ud.sh
+++ bash/train_ud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-##SBATCH -p gpu --gres=gpu:titanx:1
+#SBATCH -p gpu --gres=gpu:titanx:1
 #SBATCH --time=2-00:00:00
 
 #echo $CUDA_VISIBLE_DEVICES
@@ -11,17 +11,19 @@ lang=$1
 iso=$2
 checkpoint_dir=$3
 
+#BERT_PATH=/image/nlp-letre/bert/multi_cased_L-12_H-768_A-12/ \
 rm -rf $checkpoint_dir
 TRAIN_PATH=/image/nlp-datasets/iwpt20/train-dev/$lang/$iso-ud-train.conllu \
 DEV_PATH=/image/nlp-datasets/iwpt20/train-dev/$lang/$iso-ud-dev.conllu \
 WORD_DIM=768 \
 LOWER_CASE=FALSE \
-BATCH_SIZE=4 \
-BERT_PATH=/image/nlp-letre/bert/multi_cased_L-12_H-768_A-12/ \
+BATCH_SIZE=8 \
+BERT_PATH=/image/nlp-letre/bert/distilmulti_cased_L-6_H-768_A-12 \
 allennlp train \
 -s $checkpoint_dir \
 --include-package utils \
 --include-package modules \
 --file-friendly-logging \
-config/transition_eud.jsonnet
+config/transition_eud_gpu.jsonnet
+#config/transition_eud.jsonnet
 
