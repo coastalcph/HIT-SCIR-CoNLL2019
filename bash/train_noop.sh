6e37c79c5369386ce8307eca4aa578ef28bc80cd
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-03-18 13:54:48 +0100

    train noop does arabic by default but can take other command line args

--- bash/train_noop.sh
+++ bash/train_noop.sh
@@ -1,9 +1,12 @@
 nlp_datasets=/image/nlp-datasets
-train_set=${nlp_datasets}/iwpt20/train-dev/UD_Arabic-PADT/ar_padt-ud-train.conllu
-dev_set=${nlp_datasets}/iwpt20/train-dev/UD_Arabic-PADT/ar_padt-ud-dev.conllu
+treebank_name=${1:UD_Arabic-PADT}
+iso=${2:ar_padt}
+train_set=${nlp_datasets}/iwpt20/train-dev/$treebank_name/$iso-ud-train.conllu
+dev_set=${nlp_datasets}/iwpt20/train-dev/$treebank_name/$iso-ud-dev.conllu
 model_save_path=models/noop_eud
 config_file=config/noop_eud.jsonnet
 
+rm -rf $model_save_path
 CUDA_VISIBLE_DEVICES=-1 \
 TRAIN_PATH=${train_set} \
 DEV_PATH=${dev_set} \
