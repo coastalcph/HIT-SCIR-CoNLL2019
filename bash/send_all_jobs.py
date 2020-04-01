cc3b7a91185780b7ee6ff9f71d19d9dd2cd5cc50
Miryam de Lhoneux <mdelhoneux@gmail.com>
2020-04-01 17:42:03 +0200

    updated

--- bash/send_all_jobs.py
+++ bash/send_all_jobs.py
@@ -9,24 +9,27 @@ import os,sys
 #ISO_TODO = ['sv_talbanken', 'en_ewt', 'lv_lvtb', 'cs_fictree',
 #'cs_pdt', 'it_isdt', 'uk_iu', 'pl_pdb','ru_syntagrus',
 #'sk_snk','nl_alpino', 'nl_lassysmall', 'et_ewt']
-#ISO_TODO = ['cs_cac', 'ar_padt', 'fi_tdt']
+ISO_TODO = ['cs_cac', 'ar_padt', 'fi_tdt']
+ISO_TODO = ['ar_padt']
 #ISO_TODO += ['cs_cac', 'ar_pdt', 'fi_tdt']
-ISO_TODO=None #do everything by default
+#ISO_TODO=None #do everything by default
 
-eud_dir=sys.argv[1]
-chkpoints = sys.argv[2]
+eud_dir='/cluster/projects/nn9447k/mdelhoneux/train-dev/' #sys.argv[1]
+chkpoints = 'checkoints' #sys.argv[2]
 langs = os.listdir(eud_dir)
 for lang in langs:
-    if lang.endswith('PUD') or lang.endswith('FQB'):
-        continue
-    iso_cmd = f'ls {eud_dir}{lang}/*train.conllu'
-    iso = [line for line in os.popen(iso_cmd)][0].split("/")[6].split("-")[0]
-    #not running jobs for the ones we already have a model for
-    #if os.path.exists(f'{chkpoints}/{iso}/best.th'):
-    #    continue
-    if not ISO_TODO or iso in ISO_TODO:
-        if iso == 'et_ewt':
-            send_cmd = f'sbatch --job-name {iso} bash/train_ud.sh {lang} {iso} {chkpoints}/{iso} /image/nlp-datasets/iwpt20/et_split/'
-        else:
-            send_cmd = f'sbatch --job-name {iso} bash/train_ud.sh {lang} {iso} {chkpoints}/{iso}'
-        os.system(send_cmd)
+	if lang.endswith('PUD') or lang.endswith('FQB'):
+		continue
+	iso_cmd = f'ls {eud_dir}{lang}/*train.conllu'
+	iso = [line for line in os.popen(iso_cmd)][0].split("/")[7].split("-")[0]
+	#not running jobs for the ones we already have a model for
+	#if os.path.exists(f'{chkpoints}/{iso}/best.th'):
+	#	 continue
+	if not ISO_TODO or iso in ISO_TODO:
+		if iso == 'et_ewt':
+			#TODO: update
+			send_cmd = f'sbatch --job-name {iso} bash/train_ud_saga.sh {lang} {iso} {chkpoints}/{iso} /image/nlp-datasets/iwpt20/et_split/'
+		else:
+			send_cmd = f'sbatch --job-name {iso} bash/train_ud_saga.sh {lang} {iso} {chkpoints}/{iso}'
+		#print(send_cmd)
+		os.system(send_cmd)
