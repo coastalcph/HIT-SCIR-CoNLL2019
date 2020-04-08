8f2658bd83eb4c559355bb950f659c8cec9be43a
miryam <mdelhoneux@gmail.com>
2020-04-08 16:20:01 +0200

    small refactor

--- metrics/xud_score.py
+++ metrics/xud_score.py
@@ -36,52 +36,15 @@ class XUDScore(Metric):
 
     def get_metric(self, reset: bool = False) -> Dict[str,float]:
         if self.predictions and self.golds:
-            #predictions
-            #flatten list
-            preds = [item for sublist in self.predictions for item in sublist]
-            #hack to avoid double new lines when joining
-            preds = [pred if pred != '\n' else '' for pred in preds]
-            if self.collapse:
-                string_pred = '\n'.join(predictions)+'\n'
-                string_pred = string_pred.encode("utf-8")
-                pred_tmp_file = tempfile.NamedTemporaryFile(delete=False)
-                pred_tmp_file.write(string_pred)
-                pred_tmp_file.close()
-                try:
-                    pred_collapsed = self.collapse_conllu(pred_tmp_file.name)
-                except subprocess.CalledProcessError:
-                    return
-                preds = [pred for pred in pred_collapsed.stdout.partition("\n") ]
-                string_pred = ''.join(preds)
-            else:
-                string_pred = '\n'.join(preds)+'\n'
-
+            string_pred = self.get_string(self.predictions)
             pred_graphs = load_conllu_default(string_to_file(string_pred))
-
-            #gold
-            golds = [item for sublist in self.golds for item in sublist]
-            golds = [gold if gold != '\n' else '' for gold in golds]
-            if self.collapse:
-                string_gold = '\n'.join(golds)+'\n'
-                string_gold = string_gold.encode("utf-8")
-                gold_tmp_file = tempfile.NamedTemporaryFile(delete=False)
-                gold_tmp_file.write(string_gold)
-                gold_tmp_file.close()
-                try:
-                    gold_collapsed = self.collapse_conllu(gold_tmp_file.name)
-                except subprocess.CalledProcessError:
-                    return
-                golds = [gold for gold in gold_collapsed.stdout.partition("\n")]
-                string_gold = ''.join(golds)
-            else:
-                string_gold = '\n'.join(golds)+'\n'
+            string_gold = self.get_string(self.golds)
             gold_graphs = load_conllu_default(string_to_file(string_gold))
             results = evaluate(gold_graphs, pred_graphs)
 
         if reset:
             self.predictions = []
             self.golds = []
-        #print(results['ELAS'].f1)
         return {"ELAS":results['ELAS'].f1}
 
     @retry(stop_max_attempt_number=3)
@@ -89,4 +52,25 @@ class XUDScore(Metric):
         cmd = ["perl", self._collapse, filename]
         return subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True,
                     check=True)
+
+    def get_string(self, conllu_list):
+        #flatten list
+        conllu_list = [item for sublist in conllu_list for item in sublist]
+        #hack to avoid double new lines when joining
+        conllu_list = [conllu_item if conllu_item != '\n' else '' for conllu_item in conllu_list]
+        if self.collapse:
+            string_conllu_list = '\n'.join(conllu_list)+'\n'
+            string_conllu_list = string_conllu_list.encode("utf-8")
+            conllu_list_tmp_file = tempfile.NamedTemporaryFile(delete=False)
+            conllu_list_tmp_file.write(string_conllu_list)
+            conllu_list_tmp_file.close()
+            try:
+                conllu_list_collapsed = self.collapse_conllu(conllu_list_tmp_file.name)
+            except subprocess.CalledProcessError:
+                return
+            conllu_list = [conllu_item for conllu_item in conllu_list_collapsed.stdout.partition("\n") ]
+            string_conllu_list = ''.join(conllu_list)
+        else:
+            string_conllu_list = '\n'.join(conllu_list)+'\n'
+        return string_conllu_list
 
