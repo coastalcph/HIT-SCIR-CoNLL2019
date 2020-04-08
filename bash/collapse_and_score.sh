e5a4c248f4846f50a48bf7e572c0ad238fefff7d
Artur Kulmizev <artku750@login-1.saga.sigma2.no>
2020-04-08 09:55:09 +0200

    score script?

---
+++ bash/collapse_and_score.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+treebank_dir=/cluster/projects/nn9447k/mdelhoneux/train-dev
+treebanks=$(ls $treebank_dir)
+checkpoints=/cluster/projects/nn9447k/mdelhoneux/models/mbert
+
+for tb in $treebanks; do
+    gold=$(find $treebank_dir/${tb}/ -name *-ud-dev.conllu -exec basename {} \;)
+    tb_code=$(echo $gold | sed 's/-.*//')
+    pred=$(find ${checkpoints}/${tb_code}/ -name *preprocessed-stanza* -exec basename {} \;)
+    tmp_gold=/tmp/gold.conllu
+    tmp_pred=/tmp/pred.conllu
+    perl tools/enhanced_collapse_empty_nodes.pl ${treebank_dir}/${tb}/${gold} > $tmp_gold
+    perl tools/enhanced_collapse_empty_nodes.pl ${checkpoints}/${tb}/${pred} > $tmp_pred
+    echo $tb
+    python metrics/iwpt20_xud_eval.py $tmp_gold $tmp_pred
+done
