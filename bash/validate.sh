c9faedeb38269a32faf2153f4607e2b60b56e17f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-20 15:50:37 +0200

    Save validation.txt files for each conllu

--- bash/validate.sh
+++ bash/validate.sh
@@ -3,14 +3,16 @@
 cd $(dirname $0)/../tools
 tools=$(pwd)
 cd /cluster/projects/nn9447k/mdelhoneux/models/mbert
-rm invalid.txt
-for preprocessor in stanza udpipe; do
-  for dir in *; do
-    lang=${dir%_*}
-    echo ==== $lang ====
-    for pred in $dir/*predicted-$preprocessor-test.conllu; do
-      echo === $pred ===
-      python $tools/validate.py $pred --lang $lang || echo $pred >> invalid.txt
+for div in dev test; do
+  for preprocessor in stanza udpipe; do
+    for dir in *; do
+      lang=${dir%_*}
+      for pred in $dir/*predicted-$preprocessor-$div; do
+        echo $pred ...
+        python $tools/validate.py $pred --lang $lang --level 2 > $pred.validation.txt 2>&1 &
+      done
     done
-  done |& tee validation_$preprocessor.txt
+  done
+  wait < <(jobs -p)
+  tail -n1 /cluster/projects/nn9447k/mdelhoneux/models/mbert/*/*predicted-*-test.conllu.validation.txt
 done
