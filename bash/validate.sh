21e58d7e84a781e80a909a8616a078bd436dde67
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-20 15:57:54 +0200

    Fix missing .conllu

--- bash/validate.sh
+++ bash/validate.sh
@@ -7,7 +7,7 @@ for div in dev test; do
   for preprocessor in stanza udpipe; do
     for dir in *; do
       lang=${dir%_*}
-      for pred in $dir/*predicted-$preprocessor-$div; do
+      for pred in $dir/*predicted-$preprocessor-$div.conllu; do
         echo $pred ...
         python $tools/validate.py $pred --lang $lang --level 2 > $pred.validation.txt 2>&1 &
       done
