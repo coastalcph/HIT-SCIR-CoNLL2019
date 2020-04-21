6bfa5f907aeaa7475ffd8873fe830fff4aa09584
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-21 12:01:46 +0200

    Fix package filename

--- bash/package_submission.sh
+++ bash/package_submission.sh
@@ -2,7 +2,7 @@
 for preprocessor in stanza udpipe; do
   for div in dev test; do
     cd submission/$preprocessor/$div
-    tar cvzf ../../../koebsala-$preprocessor_$div_`date '+%Y%m%d_%H%M%S'`.tgz *.conllu
+    tar cvzf ../../../koebsala-${preprocessor}_${div}_`date '+%Y%m%d_%H%M%S'`.tgz *.conllu
     cd -
   done
 done
