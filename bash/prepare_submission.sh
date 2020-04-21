987e3f20b107415128fabd31a46fdc9cbbf8281b
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-21 10:32:21 +0200

    Evaluate dev

--- bash/prepare_submission.sh
+++ bash/prepare_submission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)/..
-mkdir -p {submission,collapsed,validation,text_without_spaces}/{stanza,udpipe,gold}/{dev,test} dev test
+mkdir -p {submission,collapsed,validation,text_without_spaces}/{stanza,udpipe,gold}/{dev,test} dev dev-gold test
 if [ -n "$GET_PRED" ]; then
   scp saga:/cluster/projects/nn9447k/mdelhoneux/models/mbert/*/*-predicted-*.conllu submission/
 fi
@@ -11,6 +11,10 @@ if [ -n "$GET_TEXT" ]; then
     tar xvzf ../iwpt2020-$div-blind.tgz
     cd ..
   done
+  wget http://ufal.mff.cuni.cz/~zeman/soubory/iwpt2020-dev-gold.tgz
+  cd dev-gold
+  tar xvzf ../iwpt2020-dev-gold.tgz
+  cd ..
 fi
 cd submission/
 for f in *.conllu; do
@@ -27,8 +31,12 @@ for f in *.conllu; do
     continue
   fi
   python ../tools/validate.py $f --lang $lang --level 2 > ../validation/$preprocessor/$div/$lang.txt 2>&1 &
-  perl ../tools/enhanced_collapse_empty_nodes.pl $f > ../collapsed/$preprocessor/$div/$lang.conllu
+  perl ../tools/enhanced_collapse_empty_nodes.pl $f > ../collapsed/$preprocessor/$div/$lang.conllu 2>/dev/null
   python ../tools/iwpt20_xud_eval.py ../collapsed/$preprocessor/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu
+  if [ $div == dev ]; then
+    perl ../tools/enhanced_collapse_empty_nodes.pl ../dev-gold/$lang.conllu > ../collapsed/gold/$div/$lang.conllu 2>/dev/null
+    python ../tools/iwpt20_xud_eval.py ../collapsed/gold/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu
+  fi
   perl ../tools/text_without_spaces.pl ../$div/$lang.txt > ../text_without_spaces/gold/$div/$lang.txt
   perl ../tools/conllu_to_text.pl $f | ../tools/text_without_spaces.pl > ../text_without_spaces/$preprocessor/$div/$lang.txt
   if ! diff -q ../text_without_spaces/{gold,$preprocessor}/$div/$lang.txt; then
