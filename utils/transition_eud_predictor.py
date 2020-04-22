47be88cbf444b8602c36b177bf4a738d859cfe5c
miryam <mdelhoneux@gmail.com>
2020-04-22 09:23:02 +0200

    fixed the sorting index solution

--- utils/transition_eud_predictor.py
+++ utils/transition_eud_predictor.py
@@ -61,20 +61,25 @@ class EUDPParserPredictor(Predictor):
 def eud_trans_outputs_into_conllu(outputs, output_null_nodes=True):
     return annotation_to_conllu(eud_trans_outputs_to_annotation(outputs), output_null_nodes)
 
-def serialize_field(field,key, output_null_nodes=True):
+def serialize_field(field, key, output_null_nodes=True):
     #bit of an overkill but let's play it safe
+    field_type_error = False
     if field == '_' or field is None:
         return '_'
     elif key in ["id", "form", "lemma", "upostag", "xpostag", "head", "deprel"]:
-        if isinstance(field, (str,int)):
+        if isinstance(field, (str,int,float)):
             return str(field)
         elif isinstance(field,tuple):
             return ''.join(str(x) for x in field)
+        else:
+            field_type_error = True
     elif key in ['feats', 'misc']:
         if isinstance(field,str):
             return field
-        else:
+        elif isinstance(field,dict):
             return '|'.join(f'{k}={v}' for k,v in field.items())
+        else:
+            field_type_error = True
     elif key == 'deps':
         if isinstance(field,str):
             if not output_null_nodes:
@@ -89,12 +94,13 @@ def serialize_field(field,key, output_null_nodes=True):
                         dep_list.append((k,''.join(str(val) for val in v)))
                     else:
                         dep_list.append((k, v[0]))
-                else:
-                    assert(isinstance(v,int))
+                elif isinstance(v,int):
                     dep_list.append((k,v))
+                else:
+                    field_type_error = True
 
             return "|".join(f"{v}:{k}" for k,v in dep_list)
-    else:
+    if field_type_error:
         raise ValueError(f"Type not known for {key}, value: {field}")
 
 NULL_NODE_ID = re.compile(r"^[0-9][0-9]*\.[1-9][0-9]*")
@@ -104,7 +110,7 @@ def annotation_to_conllu(annotation, output_null_nodes=True):
     for i, line in enumerate(annotation, start=1):
         line = [serialize_field(line.get(k), k, output_null_nodes) for k in ["id", "form", "lemma", "upostag", "xpostag", "feats", "head",
             "deprel", "deps", "misc"]]
-        if output_null_nodes or not re.match(NULL_NODE_ID, line[0]):
+        if output_null_nodes or not re.match(NULL_NODE_ID, str(line[0])):
             row = "\t".join(line)
             output_lines.append(row)
 
@@ -134,7 +140,7 @@ def eud_trans_outputs_to_annotation(outputs, output_null_nodes = True):
     if null_nodes:
         for i, node in enumerate(null_nodes, start=1):
             if output_null_nodes:
-                token_index_to_id[null_node_prefix + i] = f'{null_node_prefix}.{i}'
+                token_index_to_id[null_node_prefix + i] = float(f'{null_node_prefix}.{i}')
             else:
                 #TODO: hacky! come up with something better
                 #attach everything that is attached to a node to the last sentence item
@@ -154,32 +160,33 @@ def eud_trans_outputs_to_annotation(outputs, output_null_nodes = True):
     # 3     2.1  3
     # 4     2.2  4
 
+    #import sys; sys.stdout=sys.stdout.terminal
+    #import ipdb;ipdb.set_trace()
+    #output_annotation.append(f'{output["sent_id"]}\n')
+    #output_annotation.append(f'{output["text"]}\n')
     for i, line in enumerate(word_annotation,start=1):
 
         # Handle multiword tokens
         if multiword_map and i in multiword_map:
             output_annotation.append(multiword_map[i])
         deps =[(token_index_to_id[edge[1]], edge[2]) for edge in edge_list if token_index_to_id[edge[0]] == i ]
-        #sort by index
         deps = sorted(deps, key=lambda x:x[0])
         string_deps = "|".join([str(dep[0]) + ':' + dep[1] for dep in deps])
 
-        #deps = "|".join([str(token_index_to_id[edge[1]]) + ':' + edge[2] for edge in edge_list if token_index_to_id[edge[0]] == i])
         if not deps:
             raise ValueError(f"No edge found for {line}")
 
-        line['deps'] = deps
+        line['deps'] = string_deps
         output_annotation.append(line)
 
     if null_nodes and output_null_nodes:
         for i, node in enumerate(null_nodes, start=null_node_prefix+1):
-            deps = [str(token_index_to_id[edge[1]]) + ':' + edge[2] for edge in edge_list if edge[0] == i]
-            #TODO: fix? this will sort alphabetically
+            deps = [(token_index_to_id[edge[1]], edge[2]) for edge in edge_list if edge[0] == i]
             deps = sorted(deps, key=lambda x:x[0])
             string_deps = "|".join([str(dep[0]) + ':' + dep[1] for dep in deps])
             if not deps:
                 raise ValueError(f"No edge found for {node}")
-            row = {"id":token_index_to_id[i], "deps":deps}
+            row = {"id":token_index_to_id[i], "deps":string_deps}
             output_annotation.append(row)
 
     return output_annotation
