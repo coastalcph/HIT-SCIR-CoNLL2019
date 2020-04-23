378120dda90fa2b8952f01e894abd8b9a9832acb
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-23 13:53:01 +0200

    Remove existing outputs before preparing submission

--- bash/prepare_submission.sh
+++ bash/prepare_submission.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname $0)/.." || exit
+rm -rf collapsed validation text_without_spaces
 mkdir -p {submission,collapsed,validation,text_without_spaces}/{stanza,udpipe,gold}/{dev,test} dev dev-gold test
 if [ -n "$GET_PRED" ]; then
   scp saga:/cluster/projects/nn9447k/mdelhoneux/models/mbert/*/*-predicted-*.conllu submission/
@@ -18,7 +19,7 @@ if [ -n "$GET_TEXT" ]; then
 fi
 cd submission/ || exit
 for f in *.conllu; do
-  lang=${f%-*}
+  lang=${f%%-*}
   lang=${lang%_*}
   basename=${f%.*}
   div=${basename##*-}
@@ -37,12 +38,12 @@ for f in *.conllu; do
   sed 's/\([	|]0:\)\w*/\1root/g;s/0:root|0:root/0:root/g' $f | perl ../tools/conllu-quick-fix.pl > $preprocessor/$div/$lang.conllu
 
   python ../tools/validate.py $preprocessor/$div/$lang.conllu --lang $lang --level 2 > ../validation/$preprocessor/$div/$lang.txt 2>&1 &
-  timeout 10s perl ../tools/enhanced_collapse_empty_nodes.pl $preprocessor/$div/$lang.conllu > ../collapsed/$preprocessor/$div/$lang.conllu 2>../collapsed/$preprocessor/$div/$lang.log || cat ../collapsed/$preprocessor/$div/$lang.log
-  echo Evaluating submission/$preprocessor/$div/$lang.conllu against itself
+  timeout 10s perl ../tools/enhanced_collapse_empty_nodes.pl $preprocessor/$div/$lang.conllu > ../collapsed/$preprocessor/$div/$lang.conllu 2>../collapsed/$preprocessor/$div/$lang.log || head -v -n5 ../collapsed/$preprocessor/$div/$lang.log
+  echo -n Evaluating submission/$preprocessor/$div/$lang.conllu against itself " ... "
   python ../tools/iwpt20_xud_eval.py ../collapsed/$preprocessor/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu | grep ELAS
   if [ $div == dev ]; then
-    perl ../tools/enhanced_collapse_empty_nodes.pl ../dev-gold/$lang.conllu > ../collapsed/gold/$div/$lang.conllu 2>../collapsed/gold/$div/$lang.log || cat ../collapsed/gold/$div/$lang.log
-    echo Evaluating submission/$preprocessor/$div/$lang.conllu against dev-gold/$lang.conllu
+    perl ../tools/enhanced_collapse_empty_nodes.pl ../dev-gold/$lang.conllu > ../collapsed/gold/$div/$lang.conllu 2>../collapsed/gold/$div/$lang.log || head -v -n5 ../collapsed/gold/$div/$lang.log
+    echo -n Evaluating submission/$preprocessor/$div/$lang.conllu against dev-gold/$lang.conllu " ... "
     python ../tools/iwpt20_xud_eval.py ../collapsed/gold/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu | grep ELAS
   fi
   perl ../tools/text_without_spaces.pl ../$div/$lang.txt > ../text_without_spaces/gold/$div/$lang.txt
