f6ac5c5c3c5f22fd788727e05405ecccfa9482f9
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-23 13:08:17 +0200

    Fine-grained control for submission preparation

--- bash/prepare_submission.sh
+++ bash/prepare_submission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd $(dirname $0)/..
+cd "$(dirname $0)/.." || exit
 mkdir -p {submission,collapsed,validation,text_without_spaces}/{stanza,udpipe,gold}/{dev,test} dev dev-gold test
 if [ -n "$GET_PRED" ]; then
   scp saga:/cluster/projects/nn9447k/mdelhoneux/models/mbert/*/*-predicted-*.conllu submission/
@@ -7,16 +7,16 @@ fi
 if [ -n "$GET_TEXT" ]; then
   for div in test dev; do
     wget http://ufal.mff.cuni.cz/~zeman/soubory/iwpt2020-$div-blind.tgz
-    cd $div
+    cd $div || exit
     tar xvzf ../iwpt2020-$div-blind.tgz
     cd ..
   done
   wget http://ufal.mff.cuni.cz/~zeman/soubory/iwpt2020-dev-gold.tgz
-  cd dev-gold
+  cd dev-gold || exit
   tar xvzf ../iwpt2020-dev-gold.tgz
   cd ..
 fi
-cd submission/
+cd submission/ || exit
 for f in *.conllu; do
   lang=${f%_*}
   basename=${f%.*}
@@ -25,20 +25,24 @@ for f in *.conllu; do
   preprocessor=${basename##*-}
   basename=${basename#*_}
   treebank_suffix=${basename%%-*}
-  echo === $lang $treebank_suffix $preprocessor $div $f -> $preprocessor/$div/$lang.conllu
-  if [ $treebank_suffix != all -a -f ${lang}_all-predicted-$preprocessor-$div.conllu ]; then
+  echo "=== $lang $treebank_suffix $preprocessor $div $f -> submission/$preprocessor/$div/$lang.conllu"
+  if [ $treebank_suffix != all ] && [ -f ${lang}_all-predicted-$preprocessor-$div.conllu ]; then
     echo Skipped because a concat model exists
     continue
   fi
+  [ -n "$PREPROCESSOR" ] && [ $preprocessor != $PREPROCESSOR ] && continue
+  [ -n "$DIV" ] && [ $div != $DIV ] && continue
   # Workarounds for validation errors:
   sed 's/\([	|]0:\)\w*/\1root/g;s/0:root|0:root/0:root/g' $f | perl ../tools/conllu-quick-fix.pl > $preprocessor/$div/$lang.conllu
 
   python ../tools/validate.py $preprocessor/$div/$lang.conllu --lang $lang --level 2 > ../validation/$preprocessor/$div/$lang.txt 2>&1 &
   timeout 10s "perl ../tools/enhanced_collapse_empty_nodes.pl $preprocessor/$div/$lang.conllu" > ../collapsed/$preprocessor/$div/$lang.conllu 2>/dev/null
-  python ../tools/iwpt20_xud_eval.py ../collapsed/$preprocessor/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu
+  echo Evaluating submission/$preprocessor/$div/$lang.conllu against itself
+  python ../tools/iwpt20_xud_eval.py ../collapsed/$preprocessor/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu | grep ELAS
   if [ $div == dev ]; then
     perl ../tools/enhanced_collapse_empty_nodes.pl ../dev-gold/$lang.conllu > ../collapsed/gold/$div/$lang.conllu 2>/dev/null
-    python ../tools/iwpt20_xud_eval.py ../collapsed/gold/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu
+    echo Evaluating submission/$preprocessor/$div/$lang.conllu against dev-gold/$lang.conllu
+    python ../tools/iwpt20_xud_eval.py ../collapsed/gold/$div/$lang.conllu ../collapsed/$preprocessor/$div/$lang.conllu | grep ELAS
   fi
   perl ../tools/text_without_spaces.pl ../$div/$lang.txt > ../text_without_spaces/gold/$div/$lang.txt
   perl ../tools/conllu_to_text.pl $preprocessor/$div/$lang.conllu | ../tools/text_without_spaces.pl > ../text_without_spaces/$preprocessor/$div/$lang.txt
