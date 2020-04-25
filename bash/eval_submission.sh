db355fc21f70b7f005a08255a2a75947b0af5c92
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-04-25 10:10:26 +0200

    Skip invalid files in eval

--- bash/eval_submission.sh
+++ bash/eval_submission.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd "$(dirname $0)/.." || exit
 cd submission/ || exit
-for lang in `ls ../dev|sed 's/\.txt//'`; do
+for lang in `ls ../dev|sed -n '/\.txt/{s/\.txt//;p}'`; do
   printf "$lang\t"
   for preprocessor in stanza udpipe; do
-    python ../tools/iwpt20_xud_eval.py ../collapsed/gold/dev/$lang.conllu ../collapsed/$preprocessor/dev/$lang.conllu | grep -zPo "(?<=ELAS F1 Score: ).*"
+    python ../tools/iwpt20_xud_eval.py ../collapsed/gold/dev/$lang.conllu ../collapsed/$preprocessor/dev/$lang.conllu 2>/dev/null | grep -zPo "(?<=ELAS F1 Score: ).*"
     printf "\t"
   done
   echo
