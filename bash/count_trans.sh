60258a81a2db91ca012be4486ea8475376756374
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-06-09 12:12:13 +0200

    Script to count the number of transitions per model

---
+++ bash/count_trans.sh
@@ -0,0 +1,2 @@
+wc -l /cluster/projects/nn9447k/mdelhoneux/models/mbert/{ar,bg,cs_all,nl_all,en,et_all,fi,fr,it,lv,lt,pl_all,ru,sk,sv,ta,uk}*/vocabulary/actions.txt
+grep -c ':.*:' /cluster/projects/nn9447k/mdelhoneux/models/mbert/{ar,bg,cs_all,nl_all,en,et_all,fi,fr,it,lv,lt,pl_all,ru,sk,sv,ta,uk}*/vocabulary/actions.txt
